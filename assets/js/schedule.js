48e3313ef451db51ac5f75277b377c8565497f34
William Covington <willocov@gmail.com>
2020-10-11 20:09:42 -0700

    Add files via upload

---
+++ assets/js/schedule.js
@@ -0,0 +1,19 @@
+function addGame(date, time, subject, location){
+    console.log("Adding game: Date: " + date, ": " + time);
+    $("ul").append("<li>" + "Date: " + date + "</li>");
+}
+
+function emptyList(){
+    console.log("Clearing List Items");
+    $("ul").empty();
+}
+
+$(document).ready(function() {
+    var myItems;
+    console.log("test");
+    $.getJSON('../2020Schedule.json', function(data) {
+        myItems = data.items;
+        console.log(myItems);
+    });
+});
+
