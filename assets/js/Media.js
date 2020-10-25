2f0ccc2fd7e977ddeb606f3ee978370039f107ce
William Covington <willocov@gmail.com>
2020-10-24 19:24:44 -0700

    Finished "Media Page". Mobile Compatible

--- assets/js/Media.js
+++ assets/js/Media.js
@@ -1,10 +1,33 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
+function openCloseMenu() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
       x.style.display = "none";
     } else {
       x.style.display = "block";
     }
-  }
-  
+}
+
+function showAll(){
+    $('#HighlightCompilations').show();
+    $('#2020SeasonList').show();
+    $('#2002WorldChamps').show();
+}
+
+function show2020Season(){
+    $('#HighlightCompilations').hide();
+    $('#2020SeasonList').show();
+    $('#2002WorldChamps').hide();
+}
+
+function show2002WorldChamps(){
+    $('#HighlightCompilations').hide();
+    $('#2020SeasonList').hide();
+    $('#2002WorldChamps').show();
+}
+
+function showHightlightComp(){
+    $('#HighlightCompilations').show();
+    $('#2020SeasonList').hide();
+    $('#2002WorldChamps').hide();
+}
